d365f0ba3d6be2df86f781f7204d9daacd534ed0
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-20 16:20:45 +1100

    Tweeter App

--- Challenges/Ajax/Connect/js/main.js
+++ Challenges/Ajax/Connect/js/main.js
@@ -1,58 +1,56 @@
-//  document.querySelector("button").addEventListener("click", getUserData);
-//  document.getElementById("display").addEventListener("click", getTweets);
+ document.querySelector("button").addEventListener("click", getUserData);
+ document.getElementById("display").addEventListener("click", getTweets);
+ //document.querySelector("button").addEventListener("click", clean);
 
-// function getUserData(){
-//        // document.getElementById("root").innerHTML = "starting the request";
+function getUserData(){
+       // document.getElementById("root").innerHTML = "starting the request";
     
-//         $.ajax({
-//             url:'http://localhost:3000/tweets.json',
-//             type: 'POST',
-//             data: {tweet: {message: `${document.getElementById("tweet").value}`}},
-//             success: (data) =>{
-//                 document.getElementById("root").innerHTML = data.message;
-//             },
-//             error: (error) =>{
-//                 console.log("Error");
-//                 console.log(error);
-//             }
-//         })
-//     }
+        $.ajax({
+            url:'http://localhost:3000/tweets.json',
+            type: 'POST',
+            data: {tweet: {message: `${document.getElementById("tweet").value}`}},
+            success: (data) =>{
+                document.getElementById("root").innerHTML = data.message;
+            },
+            error: (error) =>{
+                console.log("Error");
+                console.log(error);
+            }
+        })
+    }
 
 
-// function getTweets(){
-//     //document.getElementById("root").innerHTML = "starting the request";
+function getTweets(){
 
-//     $.ajax({
-//         url: 'http://localhost:3000/tweets.json',
-//         type:'GET',
-//         success: (data) =>{
-//             //var myData = JSON.stringify(data);
-//             for(let i = 0;i < data.length;i++){
-//                 //document.getElementById("root").innerHTML = data[i].message;;
-//                 // x = data[i].message;
-//                 // console.log(x);
-//                 //console.log(x);
-//                 // $("#btn2").click(function(){
-//                 $("ol").append("<li> data[i].message </li>");
-//             }
-            
-//             // for(let i = 0;i < data.length;i++){
-//             //     console.log(data[i].message);
-//             // }
-            
-//         },
-//         error: (error) =>{
-//             console.log("Error");
-//             console.log(error);
-//         }
-//     })
+    $.ajax({
+        url: 'http://localhost:3000/tweets.json',
+        type:'GET',
+        success: (data) =>{
+            for(let i = 0;i < data.length;i++){
+                $("ol").append(`<li> ${data[i].message} </li>`);
+            }  
+        },
+        error: (error) =>{
+            console.log("Error");
+            console.log(error);
+        }
+    })
+}
+
+function tweet(){
+let x = document.getElementById("tweet").value;
+document.getElementById("root").innerHTML = x;
+}
+
+// function clean(){
+//     ///document.getElementById("root").innerHTML
+//     $( ".ol" ).empty();
 // }
-// function tweet(){
-// let x = document.getElementById("tweet").value;
-// document.getElementById("root").innerHTML = x;
-// }
-
-
+$("#clean" ).click(function() {
+    $("ol").empty();
+    $("#root").empty();
+    document.getElementById("tweet").value = " ";   
+  });
 
 
 /* CHALLENGE 9 - Object Destructuring
@@ -86,21 +84,21 @@ of all the properties of the passed object as long as that property value is a n
 hing: typeof
 */
 
-"use strict";
-var obj = {
-  x: 5,
-  y: 20,
-  z: 3,
-  a: "test",
-  b: true,
-  c:11,
-  d: "3"
-};
+// "use strict";
+// var obj = {
+//   x: 5,
+//   y: 20,
+//   z: 3,
+//   a: "test",
+//   b: true,
+//   c:11,
+//   d: "3"
+// };
 
-// Write mult() function here
-function mult(obj){
-    for (let i in obj){
-      let answer = obj[i] + "" ;  
-    }
-}
-console.log(mult(obj));
+// // Write mult() function here
+// function mult(obj){
+//     for (let i in obj){
+//       let answer = obj[i] + "" ;  
+//     }
+// }
+// console.log(mult(obj));
