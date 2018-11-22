19a3bcb6964b081300e0eb2ac95cc3738b299d69
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-22 12:01:21 +1100

    local Storage

--- Challenges/MorningChallenge/LandingPage/js/main.js
+++ Challenges/MorningChallenge/LandingPage/js/main.js
@@ -3,3 +3,7 @@ function randNumber() {
 }
 
 //Add your code under here
+document.getElementById("changeImage").addEventListener("click", function(){
+    let num = randNumber();
+    let image = https://picsum.photos/600/400?image=0
+})
