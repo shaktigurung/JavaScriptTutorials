2be1c6ea3b4946ea1e1400260b1f12a527405cba
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-23 13:07:14 +1100

    Lodash and NPM

--- modularise/file1.js
+++ modularise/file1.js
@@ -1,1 +1,13 @@
-console.log(global);
+const unchangeableNumber = 1234;
+
+ function add(x,y){
+    return x + y;
+}
+ function subtract(x,y){
+    return x - y;
+}
+
+export {unchangeableNumber, add, subtract}
+
+
+// Export default only exports one file
--- modularise/main.js
+++ modularise/main.js
@@ -1,3 +1,10 @@
-var global = "this is a global var";
+//import {unchangeableNumber, add, subtract} from './file1.js'; 
+//import * as NewMath from './file1.js'; //adding namespace 
+//import NewMath from './file1.js'; // when "export default " is used on export file
+import lodash from './lodash.js';
 
-console.log(global);
+// console.log(NewMath.add(2,6));
+// console.log(NewMath.subtract(4,2));
+// console.log(NewMath.unchangeableNumber);
+
+console.log(_.random(20));
