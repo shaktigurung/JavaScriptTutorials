b7557d7fcb87383237ed2735bb4fa53300b4f810
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-23 15:08:18 +1100

    Webpack

--- modularise/main.js
+++ modularise/main.js
@@ -1,10 +1,15 @@
 //import {unchangeableNumber, add, subtract} from './file1.js'; 
 //import * as NewMath from './file1.js'; //adding namespace 
 //import NewMath from './file1.js'; // when "export default " is used on export file
-import lodash from './lodash.js';
+ //import lodash from './lodash.js';
+//import {random} from './node_modules/lodash-es/lodash.js';
+ import _ from 'lodash';
+ import * as NewMath from './file1';
 
-// console.log(NewMath.add(2,6));
+console.log(NewMath.add(2,6));
 // console.log(NewMath.subtract(4,2));
 // console.log(NewMath.unchangeableNumber);
 
 console.log(_.random(20));
+console.log("Hello world");
+console.log("This is testing");
