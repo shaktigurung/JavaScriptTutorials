e6df3e87f8e8749e5202c2089f1b95e7d7c1e71b
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-26 09:42:25 +1100

    Webpack Challenge

--- modularise/main.js
+++ modularise/main.js
@@ -3,13 +3,23 @@
 //import NewMath from './file1.js'; // when "export default " is used on export file
  //import lodash from './lodash.js';
 //import {random} from './node_modules/lodash-es/lodash.js';
- import _ from 'lodash';
- import * as NewMath from './file1';
+//  import _ from 'lodash';
+//  import * as NewMath from './file1';
 
-console.log(NewMath.add(2,6));
-// console.log(NewMath.subtract(4,2));
-// console.log(NewMath.unchangeableNumber);
+// console.log(NewMath.add(2,6));
+// // console.log(NewMath.subtract(4,2));
+// // console.log(NewMath.unchangeableNumber);
 
-console.log(_.random(20));
-console.log("Hello world");
-console.log("This is testing");
+// console.log(_.random(20));
+// console.log("Hello world");
+// console.log("This is testing");
+
+class Person{
+    name = "Garret Bad Boi";
+    static homosapien = true;
+}
+
+let teacher = new Person();
+
+console.log(teacher.name);
+console.log(Person.homosapien);
--- modularise/webpack.config.js
+++ modularise/webpack.config.js
@@ -4,5 +4,20 @@ module.exports = {
     output: {
             filename: "main.js",
             publicPath: "dist"
+    },
+    module:{
+        rules: [
+            {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                }
+            }
+        }
+      ]
     }
 }
