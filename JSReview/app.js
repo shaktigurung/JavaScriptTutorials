11a9202f114b0f8e3b5e50aa87ab3d915536e8ab
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-27 15:02:52 +1100

    fat arrow

--- JSReview/app.js
+++ JSReview/app.js
@@ -29,7 +29,47 @@
 // minMax([100, -3, -1000]);
 
 
-
-
 //Get every nth element in an given array
+//negative nth start from the end of the array
 //nthElement ([1,2,3,4,5,6,8], 2)
+
+// let numArray = (arr, pos) => {
+
+// }
+
+
+
+//ES6 Fat arrow
+//Throw and error if either number is not a number
+//catch that error outside of the function and log it to the console
+//also throw an error for divison by 0
+
+// function divide(x,y){
+// return x/y;
+// }
+
+let divide = (x,y) => {
+    
+       if (typeof x === "number" && typeof y === "number"){
+        if (y === 0){
+            throw "Cannot have Zero as denominator";
+        }
+        else{
+            throw "Both input are Numbers";
+        }
+      }
+      else{
+          throw "Input is Not a number";   
+      }
+ 
+    let answer = x/y;
+    console.log(answer);
+}
+
+try {
+ console.log(divide(2,0));
+// divide(4,2);
+// divide(2,"8");
+} catch(err){
+    console.log(err);
+}
