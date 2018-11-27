5fe618ddfed858264de1e3d5e4b71579b61b3f55
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-27 13:56:19 +1100

    JS Review Challenges

---
+++ JSReview/app.js
@@ -0,0 +1,35 @@
+//write a Javascript program that returns the min and max values in an array
+// only numbers count
+
+//[1,2,3,4,5]
+//[100, -3, -1000]
+//["33", 5, 1]
+
+// function minMax(arr){
+//     let numArray = arr.sort();
+//     let finalArray =[];
+//     for(let i = 0; i < numArray.length; i ++){
+//         if (isNaN(numArray[i])){
+//             console.log(numArray[i] + "is Not a number");
+//             continue;
+//         }
+//         else{
+//             finalArray.push(numArray[i]);
+//         }
+       
+//     }
+//      console.log([finalArray[0],finalArray[finalArray.length - 1]]);
+//     // console.log("MinValue:" + finalArray[0]);
+//     // console.log("MaxValue:" + finalArray[finalArray.length - 1]);
+// }
+
+
+// //minMax([1,2,3,4,5]);
+// //minMax(["33", 5, 1]);
+// minMax([100, -3, -1000]);
+
+
+
+
+//Get every nth element in an given array
+//nthElement ([1,2,3,4,5,6,8], 2)
