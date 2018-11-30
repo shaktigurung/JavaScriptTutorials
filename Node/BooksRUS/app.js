c690f0fc7ed1f85c023232ac683f72be860d0f2f
shaktigurung <mr.shakti.gurung@gmail.com>
2018-11-30 12:59:30 +1100

    CRUD express done

--- Node/BooksRUS/app.js
+++ Node/BooksRUS/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require('method-override');
 const app = express();
 const port = 3000;
 //Mongoose
@@ -13,6 +14,9 @@ mongoose.connection.on("error", error => console.log(error));
 //Handlebars setup
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method', {methods: ['POST', 'GET']}));
 
 //Body-parser 
 app.use(bodyParser.urlencoded({extended: false}));
--- Node/BooksRUS/controllers/author_controller.js
+++ Node/BooksRUS/controllers/author_controller.js
@@ -1,34 +1,57 @@
-function create(req, res){
+const AuthorModel = require("./../database/models/author_model");
+
+ async function create(req, res){
     //logic for creating a resource
+    let {name, bio, gender} = req.body;
+    let author = await AuthorModel.create({name, bio, gender})
+        .catch(err => res.status(500).send(err));
+    res.redirect(`/authors/${author._id}`);
 }
 
-function index(req, res){
+async function index(req, res){
     //showed a list of all the resources
+    let authors = await AuthorModel.find();
+    res.render("author/index", {authors});
 }
 
 function make(req, res){
     //showed the form to a create the resource
+    res.render("author/new");
 }
 
-function destroy(req, res){
+async function destroy(req, res){
     //deletes the resource
+    let {id} = req.params; //or let id = req.params.id
+    await AuthorModel.findByIdAndRemove(id);
+    res.redirect("/authors");
+
 }
 
-function show(req, res){
+async function show(req, res){
     //shows a single resources
+    let {id} = req.params;
+    let author = await AuthorModel.findById(id);
+    res.render("author/show", {author});
 }
 
-function update(req, res){
+async function update(req, res){
     //Updates the resources
+    let {name, bio, gender} = req.body;
+    let{id} = req.params;
+    await AuthorModel.findByIdAndUpdate(id, {name, bio, gender});
+    res.redirect(`/authors/${id}`);
 }
 
-function edit(req, res){
+ async function edit(req, res){
     //shows the form to edit the resources
+    let {id} = req.params;
+    let author = await AuthorModel.findById(id);
+    res.render("author/edit", {author});
 }
 
 module.exports = {
     create,
-    index,s
+    index,
     make,
     destroy,
     show,
