fba9509ca07ec74b358014b70c0dab9ee6373c8b
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-10 10:53:25 +1100

    Jest Intro

--- Node/TDD/app.js
+++ Node/TDD/app.js
@@ -1,6 +1,9 @@
 //Continuous Integration Continuous Integration (CI) is a development practice that requires developers to integrate code into a shared repository several times a day. Each check-in is then verified by an automated build, allowing teams to detect problems early.
 
 function add (x, y){
+    if (!x || !y){
+        throw "2 numbers are required";
+    }
     return x + y ;
 }
 
--- Node/TDD/app.test.js
+++ Node/TDD/app.test.js
@@ -1,8 +1,17 @@
 const { add } = require("./app");
+const assert = require("assert");
 
-const result = add(1,5);
 
-if (result !== 6){
-    throw "add did not bring back 6 when given 1 and 5";
-}
+console.log("First test: add(1,5) is equal to 6");
+const actual = add(1,5);
+const expectation = 6;
+assert.equal(actual, expectation);
+console.log("First test: ran successfully");
 
+console.log("Second test: add() with 1 argument throws an error");
+assert.throws(()=>{
+    add(1);
+});
+console.log("Second test: ran successfully");
+
+console.log("All test ran successfully");
