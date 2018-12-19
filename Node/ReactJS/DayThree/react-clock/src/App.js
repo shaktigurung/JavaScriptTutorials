c5a79c431c73b05cc79f135a1293ec82a85defe4
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-19 13:11:29 +1100

    Clock

--- Node/ReactJS/DayThree/react-clock/src/App.js
+++ Node/ReactJS/DayThree/react-clock/src/App.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Clock from "./Clock";
 
-const App = (props) => {
-    return (
-        <div>
-            <Clock 
-                icon="sun.svg"
-                timezone={"Sydney/Australia"} 
-                date={new Date()} 
-            />
-        </div>
-    );
+class App extends Component{
+    state = {latitude: null, errorMessage: "", value: 1}; //Syntatical sugar
+
+    constructor(props){
+        super(props); // Helps to call parent constructor which in this case is  "Component"
+       // this.state = {latitude: null, errorMessage: ""};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {this.setState({latitude: position.coords.latitude})},
+            error => {this.setState({errorMessage: error.message })}
+        );
+
+        setInterval(()=>{
+            this.setState((state, props) =>{
+                return {value: state.value + 1};
+            });
+        }, 1000);
+
+    }
+
+    isItWarm(){
+        const {latitude} = this.state;
+        
+        if(latitude){
+            const month = new Date().getMonth();
+            //console.log(month);
+            if (
+                ( month >=4 && month <=9 && latitude >0)
+                ||
+                ((month >= 9 || month<= 4) && latitude <0)
+                ||
+                (latitude ===0)
+            ){
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    getClockIcon(){
+        if(this.isItWarm())
+        {
+            return "sun.svg";
+        }
+        return "snowflake.svg";
+    }
+
+    render(){
+        const {latitude, errorMessage, value} = this.state;
+        return (
+            <div> 
+                 {value}
+                 {this.isItWarm()}
+                
+                 {errorMessage || <Clock 
+                    icon={latitude !== null ? this.getClockIcon() : null}
+                    timezone={"Sydney/Australia"} 
+                    date={new Date()} 
+                 /> }
+            </div>
+        );
+    }
 }
 
 export default App;
