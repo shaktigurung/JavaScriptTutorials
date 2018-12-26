83a7cb948938394c4f0ec7e9fd1f2beda6a7b1d4
shaktigurung <mr.shakti.gurung@gmail.com>
2018-12-26 20:52:07 +1100

    State

--- Node/ReactJS/ReactFullCourse/reactlearning/src/components/App.js
+++ Node/ReactJS/ReactFullCourse/reactlearning/src/components/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {Component} from "react";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import MainContent from "./MainContent";
 
-
-function App(){
-    return (
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+            answer: "Yes"
+        }
+    }
+    render(){
+        return (
             <div>
+                <p>This is test class</p>
                 <NavBar />
                 <MainContent />
                 <Footer />
             </div>
-    );
+        );
+    }
+ 
 }
 
 export default App;
--- Node/ReactJS/ReactFullCourse/reactlearning/src/index.js
+++ Node/ReactJS/ReactFullCourse/reactlearning/src/index.js
@@ -2,13 +2,8 @@ import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
 import App from "./components/App";
-import ClassApp from "./components/classApp";
 
 ReactDom.render(
-    <div>
-     <App />
-     <ClassApp />
-     </div>
-     ,
+        <App />,
      document.getElementById("root")
 );
