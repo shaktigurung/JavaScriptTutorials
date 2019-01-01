6a7dd170607a6efdb2cb2c1aeee6e174d0ba6c21
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-01 17:27:56 +1100

    New Life Cycle Method

--- Node/ReactJS/ReactFullCourse/lifecyclemethodlearning/src/app.js
+++ Node/ReactJS/ReactFullCourse/lifecyclemethodlearning/src/app.js
@@ -3,7 +3,45 @@ import React, {Component} from "react";
 class App extends Component{
     constructor(){
         super();
+        this.state={};
 
+    }
+    componentDidMount(){
+        //Use to fetch api for some data
+    }
+
+    static getDerivedStateFromProps(props,state){
+        //return new, updated state based on the props
+    }
+
+    getSnapshotBeforeUpdate(){
+        //create backup of way the things are
+    }
+    //These three life Cyle Methods are deprecated
+    // componentWillMount(){
+
+    // }
+    // componentWillReceiveProps(nextProps){
+    //     //is used to receive props passed by the parent
+    //     //this method will be deprecated soon
+
+    //     if(nextProps.whatever !== this.props.whatever){
+    //         //Do something important here
+    //     }
+    // }
+    // componentWillUpdate(){
+
+    // }
+
+    shouldComponentUpdate(nextProps, nextState){
+        //return true if need to be updated
+        //return false if not
+
+    }
+
+    componentWillUnmount(){
+        //teardown or cleanup code before component dissappears
+        //example: remove event listener
     }
     render(){
         return(
