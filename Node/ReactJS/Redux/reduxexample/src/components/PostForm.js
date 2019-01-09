bdbb12f18b8d9d6ad33475fb42b1feb7fa2ed010
shaktigurung <mr.shakti.gurung@gmail.com>
2019-01-09 15:58:00 +1100

    Redux practises setup

---
+++ Node/ReactJS/Redux/reduxexample/src/components/PostForm.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class PostForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            title:'',
+            body:''
+         }
+    }
+    onChange = (event)=> {
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    }
+
+    onSubmit = (event)=>{
+        event.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: 'POST',
+            headers: {
+                'content-type':'application/json' 
+            },
+            body: JSON.stringify(post)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
+    render() { 
+        return (
+            <div>
+                <h1> Add forms </h1>
+                <form onSubmit = {this.onSubmit}>
+                    <div>
+                        <label>Title</label>
+                        <br/>
+                        <input type="text" name="title" onChange = {this.onChange} value={this.state.title} />
+                    </div>
+                    <br/>
+                    <div>
+                        <label>Body</label>
+                        <br/>
+                        <textarea name="body" onChange = {this.onChange} value={this.state.body}></textarea>
+                    </div>
+                    <br/>
+                    <button type="submit">Submit </button>
+                </form>
+            </div>
+          );
+    }
+}
+ 
+export default PostForm;
